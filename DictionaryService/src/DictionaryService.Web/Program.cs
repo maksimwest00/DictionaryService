ae44225d49b509fcf54089d55ad1922ef220ae50
maksimwest00 <maksim.west00@gmail.com>
2025-12-15 18:37:00 +0300

    Исправление ошибок при мерже

--- DictionaryService/src/DictionaryService.Web/Program.cs
+++ DictionaryService/src/DictionaryService.Web/Program.cs
@@ -23,38 +23,9 @@ try
 }
 catch (Exception ex)
 {
-<<<<<<< Updated upstream
-    app.MapOpenApi();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/openapi/v1.json", "DictionaryService");
-    });
-=======
-    Log.Information("Starting web application");
-
-    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-    string environment = builder.Environment.EnvironmentName;
-
-    builder.Configuration.AddJsonFile(
-        $"appsettings.{environment}.json",
-        true,
-        true);
-
-    builder.Services.AddProgramDependencies(builder.Configuration);
-
-    WebApplication app = builder.Build();
-
-    app.Configure();
-
-    app.Run();
-}
-catch (Exception ex)
-{
     Log.Fatal(ex, "Application terminated unexpectedly");
 }
 finally
 {
     Log.CloseAndFlush();
->>>>>>> Stashed changes
 }

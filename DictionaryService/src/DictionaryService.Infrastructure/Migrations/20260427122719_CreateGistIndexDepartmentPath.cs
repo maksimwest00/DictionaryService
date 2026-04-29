949b2d96e71744722dfe8e663342e1fbc0cae191
maksimwest00 <maksim.west00@gmail.com>
2026-04-29 15:13:35 +0300

    Добавление gist индекса

---
+++ DictionaryService/src/DictionaryService.Infrastructure/Migrations/20260427122719_CreateGistIndexDepartmentPath.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DictionaryService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateGistIndexDepartmentPath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE INDEX idx_departments_path ON departments USING gist(path);");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP INDEX idx_departments_path;");
+        }
+    }
+}

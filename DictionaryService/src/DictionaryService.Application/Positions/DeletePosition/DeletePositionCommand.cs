97188076e02a1acada7e48b133128f0c32edc17e
maksimwest00 <maksim.west00@gmail.com>
2026-09-04 11:24:39 +0300

    Реализация фичи "Удалить должность" через soft delete ( через флаг IsActive )

---
+++ DictionaryService/src/DictionaryService.Application/Positions/DeletePosition/DeletePositionCommand.cs
@@ -0,0 +1,13 @@
+﻿using DictionaryService.Application.Abstractions;
+
+namespace DictionaryService.Application.Positions.DeletePosition;
+
+public record DeletePositionCommand : ICommand
+{
+    public Guid Id { get; }
+
+    public DeletePositionCommand(Guid id)
+    {
+        Id = id;
+    }
+}
